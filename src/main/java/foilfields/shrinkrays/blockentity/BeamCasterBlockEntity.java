18e4473f858cdcaee84656450b2d2cc7b272b6df
woukie <lucaseldon@live.com>
2023-07-26 20:07:18 +0100

    Added documentation and set mod version

--- src/main/java/foilfields/shrinkrays/blockentity/BeamCasterBlockEntity.java
+++ src/main/java/foilfields/shrinkrays/blockentity/BeamCasterBlockEntity.java
@@ -17,21 +17,38 @@ import net.minecraft.world.World;
 
 import java.util.List;
 
-// Only exists because blocks don't have a tick method >:(
+/**
+ * The BlockEntity class for the Beam Caster block. Responsible for handling the behavior and interactions of the block.
+ */
 public class BeamCasterBlockEntity extends BlockEntity {
+    /**
+     * Creates a new BeamCasterBlockEntity instance.
+     *
+     * @param pos   The position of the block entity.
+     * @param state The state of the block entity.
+     */
     public BeamCasterBlockEntity(BlockPos pos, BlockState state) {
         super(ShrinkRays.BEAM_CASTER_BLOCK_ENTITY, pos, state);
     }
 
+    /**
+     * The tick method called periodically for the Beam Caster block.
+     *
+     * @param world The world where the block entity exists.
+     * @param pos   The position of the block entity.
+     * @param state The state of the block entity.
+     * @param be    The instance of the BeamCasterBlockEntity.
+     */
     public static void tick(World world, BlockPos pos, BlockState state, BeamCasterBlockEntity be) {
-        if (!state.get(AbstractBeamCaster.POWERED)) return;
+        if (!state.get(AbstractBeamCaster.POWERED))
+            return;
+
         Direction direction = state.get(AbstractBeamCaster.FACING);
-
-        Box area = new Box(pos.add(direction.getVector()), pos.add(direction.getVector().add(1,1,1)));
-
+        Box area = new Box(pos.add(direction.getVector()), pos.add(direction.getVector().add(1, 1, 1)));
         Vec3d center = area.getCenter();
 
-        if (Math.random() < 0.1f) world.playSound(null, pos, SoundEvents.ITEM_CROSSBOW_LOADING_START, SoundCategory.BLOCKS, 0.5f, 1.0f);
+        if (Math.random() < 0.1f)
+            world.playSound(null, pos, SoundEvents.ITEM_CROSSBOW_LOADING_START, SoundCategory.BLOCKS, 0.5f, 1.0f);
 
         if (!world.isClient()) {
             ((ServerWorld) world).spawnParticles(ParticleTypes.SMOKE, center.getX(), center.getY(), center.getZ(), 1, 0.25, 0.25, 0.25, 0);
@@ -40,8 +57,11 @@ public class BeamCasterBlockEntity extends BlockEntity {
         List<Entity> entities = world.getOtherEntities(null, area);
 
         entities.forEach(entity -> {
-            if (Math.random() < 0.5f) world.playSound(null, pos, SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS, 0.5f, 1.0f);
-            ((AbstractBeamCaster)state.getBlock()).onHitEntity(entity);
+            if (Math.random() < 0.5f)
+                world.playSound(null, pos, SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS, 0.5f, 1.0f);
+
+            ((AbstractBeamCaster) state.getBlock()).onHitEntity(entity);
+
             if (!world.isClient()) {
                 ((ServerWorld) world).spawnParticles(ParticleTypes.CRIT, center.getX(), center.getY(), center.getZ(), 1, 0.25, 0.25, 0.25, 0);
             }
--- src/main/java/foilfields/shrinkrays/blocks/GrowthRay.java
+++ src/main/java/foilfields/shrinkrays/blocks/GrowthRay.java
@@ -4,15 +4,28 @@ import net.minecraft.entity.Entity;
 import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleTypes;
 
+/**
+ * A GrowthRay block that extends the AbstractBeamCaster class. It can produce and control beams that can increase the size of entities.
+ */
 public class GrowthRay extends AbstractBeamCaster {
+    /**
+     * Creates a new GrowthRay instance.
+     *
+     * @param settings The settings for the GrowthRay block.
+     */
     public GrowthRay(Settings settings) {
         super(settings);
     }
 
+    /**
+     * Called when the GrowthRay beam hits an entity. Increases the size of the entity's scale.
+     *
+     * @param entity The entity hit by the GrowthRay beam.
+     */
     @Override
     public void onHitEntity(Entity entity) {
+        // Get the ScaleData of the entity and set the new scale value.
         ScaleData scaleData = ScaleData.Builder.create().entity(entity).type(ScaleTypes.BASE).build();
-
         scaleData.setScale(Math.min(scaleData.getScale() + 0.003f, 3));
     }
 }
